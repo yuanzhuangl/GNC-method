5cac4be3d6a282e26297aaa1610a234b9ec2c45b
庄园 <zhuangyuan@zhuangyuandeMacBook-Pro.local>
2017-02-16 23:13:54 -0400

    newest version

--- GNC.pl
+++ GNC.pl
@@ -6,14 +6,12 @@ use Text::Ngrams;
 
 my %d_hash;
 my @file_name;
-my @Output;
 while(<>){
     while(/^(\S+?):\s*(.*)$/gi){
         my $n = 3;
         my $ng = Text::Ngrams->new( windowsize => $n);
         $ng->process_text($2);
         my %a = $ng->get_ngrams( n=>$n, normalize => 1 );
-        my @k = keys %a;
         $d_hash{$1} = \%a;
         #create a hash whose key is the document name and value is its Ngrams and frequencies
         push @file_name, $1;
@@ -23,29 +21,18 @@ while(<>){
 while(scalar @file_name != 0){
     my $d1_name = shift @file_name;
     my @file_name_2 = @file_name;
-    my @d1_character = keys $d_hash{$d1_name};
     while(scalar @file_name_2 != 0){
         my $d2_name = shift @file_name_2;
-        my @d2_character = keys $d_hash{$d2_name};
-        my %merge = map {$_ => 1} @d1_character,@d2_character;
-        my @merge = keys (%merge);#generate the union set of Ngrams
-        
-        my @f1;
-        my @f2;
-        foreach(@merge){
-            if (exists $d_hash{$d1_name}{$_}){push @f1,$d_hash{$d1_name}{$_} ;}
-            else{push @f1,0;}
-            if (exists $d_hash{$d2_name}{$_}){push @f2,$d_hash{$d2_name}{$_} ;}
-            else{push @f2,0;}
-        }# array of two files' ngrams frequencies
-        
+        my %hash;
+        my @character =grep{++$hash{$_}<2}(keys $d_hash{$d1_name},keys $d_hash{$d2_name});
         my $result = 0;
-        for(my $i=0;$i<scalar @f1;$i++){
-            
-            my $temp = (2*($f1[$i]-$f2[$i])/($f1[$i]+$f2[$i]));
-            $result += ($temp*$temp);
+        foreach(@character){
+            my $f1=0;
+            my $f2=0;
+            $f1 = $d_hash{$d1_name}{$_} if exists $d_hash{$d1_name}{$_};
+            $f2 = $d_hash{$d2_name}{$_} if exists $d_hash{$d2_name}{$_};
+            $result += (2*($f1-$f2)/($f1+$f2))*(2*($f1-$f2)/($f1+$f2));
         }#calculate CNG distance
-        push @Output, "$d1_name $d2_name: $result\n";
+        print "$d1_name $d2_name: $result\n";
     }
 }
-print @Output;
